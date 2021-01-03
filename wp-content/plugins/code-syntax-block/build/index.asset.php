d0896323f7432138cf2f002e021d7b168ff4722d
Michelle Blanchette <michelle@purpleturtlecreative.com>
2021-01-03 14:39:19 -0500

    Update plugins

--- wp-content/plugins/code-syntax-block/build/index.asset.php
+++ wp-content/plugins/code-syntax-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '59ef346e66b0ac44096a95a27c0ca29a');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'da8fb2324d5745d9943d066f81b04745');
