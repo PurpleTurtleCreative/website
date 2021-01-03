73a1b0b423f05450844ed24a6d4d64a714f85d9d
Michelle Blanchette <michelle@purpleturtlecreative.com>
2021-01-03 15:42:46 -0500

    Update HelloElementor theme

--- wp-content/themes/hello-elementor/functions.php
+++ wp-content/themes/hello-elementor/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'HELLO_ELEMENTOR_VERSION', '2.2.0' );
+define( 'HELLO_ELEMENTOR_VERSION', '2.3.1' );
 
 if ( ! isset( $content_width ) ) {
 	$content_width = 800; // Pixels.
@@ -61,6 +61,11 @@ if ( ! function_exists( 'hello_elementor_setup' ) ) {
 			 * Editor Style.
 			 */
 			add_editor_style( 'editor-style.css' );
+			
+			/*
+			 * Gutenberg wide images.
+			 */
+			add_theme_support( 'align-wide' );
 
 			/*
 			 * WooCommerce.
