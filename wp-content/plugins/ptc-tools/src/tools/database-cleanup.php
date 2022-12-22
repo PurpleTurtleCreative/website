331a14c853fa3702b3257ae9d017b28eb27a6b60
Michelle Blanchette <michelle@purpleturtlecreative.com>
2022-12-22 17:45:56 -0500

    data cleanup functions to purge unregistered taxonomy and post type data

--- wp-content/plugins/ptc-tools/src/tools/database-cleanup.php
+++ wp-content/plugins/ptc-tools/src/tools/database-cleanup.php
@@ -30,45 +30,47 @@ elseif ( ! empty( $_POST['ptc_tool_audit_db'] ) ) :
 	print_pre( get_inactive_taxonomies(), 'Inactive Taxonomies' );
 	print_pre( get_inactive_taxonomy_terms(), 'Inactive Taxonomy Terms' );
 
-	print_hr();
+	echo '<hr />';
 
 	print_pre( get_present_post_types(), 'Present Post Types' );
 	print_pre( get_registered_post_types(), 'Registered Post Types' );
 	print_pre( get_inactive_post_types(), 'Inactive Post Types' );
-	print_pre( get_inactive_posts(), 'Inactive Post Type Posts' );
+	print_pre( get_inactive_post_ids(), 'Inactive Post Type Posts' );
 	?>
 
 	<form method="POST">
 		<input class="button button-primary" type="submit" name="ptc_tool_clean_db" value="Clean Database" />
 	</form>
 
-<?php elseif ( ! empty( $_POST['ptc_tool_clean_db'] ) ) : ?>
-
-	<pre>
-		Cleanup information
-		will go here
-		to display the general situation
-
-		You can then leave this page
-	</pre>
-
 	<?php
+elseif ( ! empty( $_POST['ptc_tool_clean_db'] ) ) :
+
+	/*
+	Post data is purged BEFORE taxonomy data by intention.
+	This is because the post data purging uses official WordPress
+	functions to clean up data references while the taxonomy data
+	purging is handled via custom queries. The WordPress functions
+	are more thorough, so they should run before custom data
+	manipulation to ensure reliability.
+	*/
+
+	print_pre( purge_inactive_post_type_data(), 'Purged Inactive Post Type Data: Deleted Posts' );
+
+	print_pre(
+		purge_inactive_taxonomy_data(),
+		'Purged Inactive Taxonomy Data: Deleted Table Rows'
+	);
+
+	echo '<a class="button" href="" target="_self">Done</a>';
 endif;
 
 ////////////////////////////////////
 ///////// HELPER FUNCTIONS /////////
 ////////////////////////////////////
 
-function print_hr() {
-	echo '<hr />';
-}
-
-function print_pre( $data, $heading_text = '' ) {
-	if ( $heading_text ) {
-		echo '<h2>' . esc_html( $heading_text ) . '</h2>';
-	}
-	echo '<pre>' . esc_html( print_r( $data, true ) ) . '</pre>';
-}
+/////////////////////////
+// INACTIVE TAXONOMIES //
+/////////////////////////
 
 function get_present_taxonomies() {
 	global $wpdb;
@@ -94,18 +96,70 @@ function get_inactive_taxonomy_terms() {
 	global $wpdb;
 
 	$inactive_taxonomies = get_inactive_taxonomies();
-	$inactive_taxonomies_sqlcsv = '\'' . implode( '\',\'', $inactive_taxonomies ) . '\'';
+	$inactive_taxonomies_sql = array_to_sql_in_clause_values( $inactive_taxonomies );
 
 	$inactive_terms = $wpdb->get_results(
 		"
 		SELECT term_taxonomy_id, term_id, taxonomy
 		FROM {$wpdb->term_taxonomy}
-		WHERE taxonomy IN({$inactive_taxonomies_sqlcsv})
-		"
+		WHERE taxonomy IN({$inactive_taxonomies_sql})
+		",
+		\ARRAY_A
 	);
 
 	return $inactive_terms;
 }
+
+function purge_inactive_taxonomy_data() {
+	global $wpdb;
+
+	$inactive_taxonomy_terms = get_inactive_taxonomy_terms();
+
+	$inactive_term_taxonomy_ids_sql = array_to_sql_in_clause_values(
+		get_distinct_column_values(
+			$inactive_taxonomy_terms,
+			'term_taxonomy_id'
+		)
+	);
+
+	$inactive_term_ids_sql = array_to_sql_in_clause_values(
+		get_distinct_column_values(
+			$inactive_taxonomy_terms,
+			'term_id'
+		)
+	);
+
+	$inactive_taxonomies_sql = array_to_sql_in_clause_values(
+		get_distinct_column_values(
+			$inactive_taxonomy_terms,
+			'taxonomy'
+		)
+	);
+
+	$deleted_table_rows = array();
+
+	$deleted_table_rows[ $wpdb->term_relationships ] = $wpdb->query(
+		"DELETE FROM {$wpdb->term_relationships} WHERE term_taxonomy_id IN({$inactive_term_taxonomy_ids_sql})"
+	);
+
+	$deleted_table_rows[ $wpdb->termmeta ] = $wpdb->query(
+		"DELETE FROM {$wpdb->termmeta} WHERE term_id IN({$inactive_term_ids_sql})"
+	);
+
+	$deleted_table_rows[ $wpdb->terms ] = $wpdb->query(
+		"DELETE FROM {$wpdb->terms} WHERE term_id IN({$inactive_term_ids_sql})"
+	);
+
+	$deleted_table_rows[ $wpdb->term_taxonomy ] = $wpdb->query(
+		"DELETE FROM {$wpdb->term_taxonomy} WHERE taxonomy IN({$inactive_taxonomies_sql})"
+	);
+
+	return $deleted_table_rows;
+}
+
+/////////////////////////
+// INACTIVE POST TYPES //
+/////////////////////////
 
 function get_present_post_types() {
 	global $wpdb;
@@ -127,21 +181,55 @@ function get_inactive_post_types() {
 	);
 }
 
-function get_inactive_posts() {
+function get_inactive_post_ids() {
 	global $wpdb;
 
 	$inactive_post_types = get_inactive_post_types();
-	$inactive_post_types_sqlcsv = '\'' . implode( '\',\'', $inactive_post_types ) . '\'';
+	$inactive_post_types_sql = array_to_sql_in_clause_values( $inactive_post_types );
 
-	$inactive_posts = $wpdb->get_col(
+	$inactive_post_ids = $wpdb->get_col(
 		"
 		SELECT ID
 		FROM {$wpdb->posts}
-		WHERE post_type IN({$inactive_post_types_sqlcsv})
+		WHERE post_type IN({$inactive_post_types_sql})
 		"
 	);
 
-	return $inactive_posts;
+	return $inactive_post_ids;
 }
 
-function delete_inactive_post_types() {}
+function purge_inactive_post_type_data() {
+
+	$deleted_posts_count = 0;
+
+	foreach ( get_inactive_post_ids() as $post_id ) {
+		if ( wp_delete_post( $post_id, true ) ) {
+			++$deleted_posts_count;
+		}
+	}
+
+	return $deleted_posts_count;
+}
+
+///////////////////////
+// GENERIC UTILITIES //
+///////////////////////
+
+function print_pre( $data, $heading_text = '' ) {
+	if ( $heading_text ) {
+		echo '<h2>' . esc_html( $heading_text ) . '</h2>';
+	}
+	echo '<pre>' . esc_html( print_r( $data, true ) ) . '</pre>';
+}
+
+function array_to_sql_in_clause_values( array $values ) {
+	return '\'' . implode( '\',\'', array_map( 'esc_sql', $values ) ) . '\'';
+}
+
+function get_distinct_column_values( array $rows, string $column_key ) {
+	return array_values(
+		array_unique(
+			array_column( $rows, $column_key )
+		)
+	);
+}
