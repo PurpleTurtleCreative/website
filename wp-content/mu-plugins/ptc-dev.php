e635886b533351fe396cc2ef2f5afc06ced11325
Michelle Blanchette <michelle@purpleturtlecreative.com>
2025-01-17 23:28:34 -0500

    tidy ptc-dev scripts, disable licensing provider override since we should most often be testing against production at this point

--- wp-content/mu-plugins/ptc-dev.php
+++ wp-content/mu-plugins/ptc-dev.php
@@ -3,9 +3,16 @@
  * Plugin Name: PTC Dev Environment
  */
 
+namespace Purple_Turtle_Creative\Dev_Env;
+
 if ( 'production' === wp_get_environment_type() ) {
 	return; // This plugin's functionality does not apply to production.
 }
+
+add_action( 'login_enqueue_scripts', __NAMESPACE__ . '\handle_login_enqueue_scripts', 999 );
+add_filter( 'authenticate', __NAMESPACE__ . '\handle_authenticate', 999, 3 );
+add_filter( 'http_request_timeout', __NAMESPACE__ . '\handle_http_request_timeout', 999, 1 );
+// add_filter( 'ptc_completionist_pro_licensing_provider_api_url', __NAMESPACE__ . '\handle_ptc_completionist_pro_licensing_provider_api_url', 999, 1 );
 
 // //////////////////////////////////////////////////// //
 
@@ -14,17 +21,23 @@ if ( 'production' === wp_get_environment_type() ) {
  *
  * @see wp-content/plugins/wordfence/modules/login-security/classes/controller/wordfencels.php
  */
-add_action( 'login_enqueue_scripts', function() {
+function handle_login_enqueue_scripts() {
 	wp_dequeue_script( 'wordfence-ls-login' );
 	wp_dequeue_style( 'wordfence-ls-login' );
-}, 999 );
+}
 
 /**
  * Authenticate all login requests if username is valid. Password can be blank.
  *
  * @see https://usersinsights.com/wordpress-user-login-hooks/
+ *
+ * @param null|\WP_User|\WP_Error $user \WP_User if the user is authenticated. \WP_Error or null otherwise.
+ * @param string                  $username Username or email address.
+ * @param string                  $password User password.
+ *
+ * @return null|\WP_User|\WP_Error
  */
-add_filter( 'authenticate', function( $user, $username, $password ) {
+function handle_authenticate( $user, $username, $password ) {
 	if ( ! is_a( $user, '\WP_User' ) && $username ) {
 		$user = get_user_by( 'login', $username );
 		if ( ! is_a( $user, '\WP_User' ) ) {
@@ -32,27 +45,34 @@ add_filter( 'authenticate', function( $user, $username, $password ) {
 		}
 	}
 	return $user;
-}, 999, 3 );
+}
 
 /**
  * Prevent cURL error 28: Resolving timed out errors in local env.
  *
  * @link https://developer.wordpress.org/reference/hooks/http_request_timeout/
+ *
+ * @param int $timeout_seconds The timeout duration.
+ *
+ * @return int
  */
-add_filter( 'http_request_timeout', function ( $timeout_seconds ) {
+function handle_http_request_timeout( $timeout_seconds ) {
 	$timeout_seconds = 10;
 	return $timeout_seconds;
-}, 999, 1 );
-
+};
 
 /**
- * Use local development domain.
+ * Use local development domain for testing ptc-resources-server changes.
+ *
+ * @param string $url The licensing provider endpoint URL.
+ *
+ * @return string
  */
-add_filter( 'ptc_completionist_pro_licensing_provider_api_url', function ( $url ) {
+function handle_ptc_completionist_pro_licensing_provider_api_url( $url ) {
 	$url = str_replace(
 		'https://purpleturtlecreative.com/',
 		'http://localhost/',
 		$url
 	);
 	return $url;
-}, 999, 1 );
+}
