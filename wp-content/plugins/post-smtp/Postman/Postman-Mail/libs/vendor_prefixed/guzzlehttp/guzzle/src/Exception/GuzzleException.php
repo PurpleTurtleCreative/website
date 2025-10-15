69a046afb3660bd9894b975d27b4d3cf1d4516ef
Michelle Blanchette <michelle@purpleturtlecreative.com>
2025-10-15 00:35:02 -0400

    uninstall plugins/post-smtp

--- wp-content/plugins/post-smtp/Postman/Postman-Mail/libs/vendor_prefixed/guzzlehttp/guzzle/src/Exception/GuzzleException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace PostSMTP\Vendor\GuzzleHttp\Exception;
-
-use PostSMTP\Vendor\Psr\Http\Client\ClientExceptionInterface;
-interface GuzzleException extends \PostSMTP\Vendor\Psr\Http\Client\ClientExceptionInterface
-{
-}
--- wp-content/plugins/post-smtp/Postman/Postman-Mail/libs/vendor_prefixed/guzzlehttp/guzzle/src/functions_include.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace PostSMTP\Vendor;
-
-// Don't redefine the functions if included multiple times.
-if (!\function_exists('PostSMTP\\Vendor\\GuzzleHttp\\describe_type')) {
-    require __DIR__ . '/functions.php';
-}
