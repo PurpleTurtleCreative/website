9a9535714abac20f223a8bf59ffcb1ba67454db6
Michelle Blanchette <michelle@purpleturtlecreative.com>
2026-03-27 18:41:21 -0400

    optimize image, hard-code copyright year to avoid SSR warning

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <footer className="component-Footer w-full bg-black text-white text-center sm:text-left">
             <div className="content-section flex flex-wrap items-center justify-center sm:justify-between gap-y-3 py-5 text-sm">
-                <p>{`©${new Date().getFullYear()} Purple Turtle Creative, LLC. All rights reserved.`}</p>
+                <p>&copy;2026 Purple Turtle Creative, LLC. All rights reserved.</p>
                 <ul className="list-none flex flex-wrap gap-x-5 gap-y-3 max-sm:justify-center">
                     <li><BuildingIcon className="inline-block relative bottom-[0.1em]" aria-label="headquarters" width="1.3em" height="1.3em" /> New York, USA</li>
                     <li><ClockIcon className="inline-block relative bottom-[0.1em]" aria-label="business local time" width="1.3em" height="1.3em" /> <Clock /></li>
