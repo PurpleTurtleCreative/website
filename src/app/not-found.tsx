05abfc6c1acf1115132f160f0e454a5289878774
Michelle Blanchette <michelle@purpleturtlecreative.com>
2026-03-27 18:26:54 -0400

    mobile tweaks

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function NotFound() {
     return (
         <main className="flex-1 flex flex-col align-center justify-center pt-26 bg-linear-170 from-primary-light from-0% to-primary to-50% text-white text-center">
-            <div className="content-section mb-[20vh]">
+            <div className="content-section sm:mb-[20dvh]">
                 <h1 className="font-heading my-5 text-shadow-lg text-shadow-primary-dark/30 text-[#9eaafb]">Error <span className="text-white">Not Found</span></h1>
                 <p className="text-lg sm:text-xl">Oops! It seems you stumbled upon some content which has since been removed.</p>
                 <p className="bg-primary-lightest text-primary-dark text-sm px-3 py-2 rounded-lg mt-3 w-fit mx-auto"><InfoIcon className="inline-block relative bottom-[0.1em]" width="1.3em" height="1.3em" />&ensp;In early 2026, we closed some projects, shifted priorities, and redesigned our website.</p>
